df2e57479a8e73823c810b1efce573c2ebe03e8e
Salsabel-Sayed <slsabilkamal99@gmail.com>
2024-11-08 13:54:54 +0200

    cors

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,12 @@ import { bootstrap } from "./bootstrap";
 import cors from "cors"
 import dotenv from "dotenv"
 import path from "path";
+
 dotenv.config()
 
 dbConnection()
 const app = express();
+app.use(cors())
 const port = process.env.PORT || 3000
 app.use(express.json())
 
@@ -18,12 +20,12 @@ app.use('*',(req,res,next)=>{
     next(new AppErrors(`route not found ${req.originalUrl}`, 404))
 })
 
-const corsOptions = {
-  origin: 'http://localhost:3000',  // Allow requests only from this origin
-  methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'], // Allowed HTTP methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
-};
-app.use(cors(corsOptions))
+// const corsOptions = {
+//   origin: 'http://localhost:3000',  // Allow requests only from this origin
+//   methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'], // Allowed HTTP methods
+//   allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+// };
+
 app.set("views",path.resolve()+"/views")
 app.set("views engine","ejs")
 app.use(express.static(path.join(path.resolve(),"public")))
